15f3fee202a88ec7d684906fdeb34502935188b3
green7 <sohnf90@yahoo.com>
2020-12-24 00:43:27 -0800

    Added the addBooktoLibrary function that will take the inputs submittted and add a card to the display with the book's information

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,25 @@ function Book(title, author, pages, read){
     }
 }
 
-function addBookToLibrary(book){
-    myLibrary.push(book);
+function addBookToLibrary(event){
+    let bookTitle = document.getElementById("title").value;
+    let bookAuthor = document.getElementById("author").value;
+    let bookPages = document.getElementById("pages").value;
+    let bookRead = document.getElementById("read").value;
+    let newBook = new Book(bookTitle, bookAuthor, bookPages, bookRead);
+    myLibrary.push(newBook);
+    // console.log(`newBook = ${newBook.info()}`)
+    let bookCard = document.createElement("div");
+    bookCard.style.marginBottom = "10px";    
+    bookCard.style.border = "1px solid black"
+    bookCard.style.padding = "5px"
+    bookCard.innerHTML = 
+        `Title: ${newBook.title}<br />
+        Author: ${newBook.author}<br />
+        Pages: ${newBook.pages}<br />
+        Read: ${newBook.read}`
+    bookDisplay.append(bookCard);
+    event.preventDefault();
 }
 
 let bookDisplay = document.querySelector(".book-display");
@@ -22,7 +39,8 @@ let bookDisplay = document.querySelector(".book-display");
 myLibrary.forEach(book => {
     let bookCard = document.createElement("div");
     bookCard.style.marginBottom = "10px";    
-    bookCard.style.border = "1px solid black"
+    bookCard.style.border = "1px solid black";
+    bookCard.style.padding = "5px";
     bookCard.innerHTML = 
         `Title: ${book.title}<br />
         Author: ${book.author}<br />
@@ -30,3 +48,6 @@ myLibrary.forEach(book => {
         Read: ${book.read}`
     bookDisplay.append(bookCard);
 })
+
+let form = document.querySelector(".inputs");
+form.addEventListener("submit", addBookToLibrary, true);
