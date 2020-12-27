3ab5716fdf1a29bb9759c63b1286a766101be90f
green7 <sohnf90@yahoo.com>
2020-12-27 01:12:20 -0800

    Added an index property to each book in the library. Reduced repetition by editing existing code into the populateDisplay function. Added a remove button for each button. Indexes are updated when a book is removed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 let myLibrary = [
-    {title: "Overwatch", author: "Jeff", pages: 100, read: "Read"},
-    {title: "Fall Guys", author: "Head Bean", pages: 200, read: "Unread"}
+    {title: "Overwatch", author: "Jeff", pages: 100, read: "Read", index: 1},
+    {title: "Fall Guys", author: "Head Bean", pages: 200, read: "Unread", index: 2}
 ];
 
-function Book(title, author, pages, read){
+function Book(title, author, pages, read, index){
     this.title = title,
     this.author = author,
     this.pages = pages,
     this.read = read,
+    this.index = index,
     this.info = function(){
         return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
     }
@@ -18,36 +19,52 @@ function addBookToLibrary(event){
     let bookAuthor = document.getElementById("author").value;
     let bookPages = document.getElementById("pages").value;
     let bookRead = document.getElementById("read").value;
-    let newBook = new Book(bookTitle, bookAuthor, bookPages, bookRead);
+    let bookIndex = myLibrary.length + 1;
+    let newBook = new Book(bookTitle, bookAuthor, bookPages, bookRead, bookIndex);
     myLibrary.push(newBook);
-    // console.log(`newBook = ${newBook.info()}`)
-    let bookCard = document.createElement("div");
-    bookCard.style.marginBottom = "10px";    
-    bookCard.style.border = "1px solid black"
-    bookCard.style.padding = "5px"
-    bookCard.innerHTML = 
-        `Title: ${newBook.title}<br />
-        Author: ${newBook.author}<br />
-        Pages: ${newBook.pages}<br />
-        Read: ${newBook.read}`
-    bookDisplay.append(bookCard);
+    populateDisplay();
     event.preventDefault();
 }
 
 let bookDisplay = document.querySelector(".book-display");
 
-myLibrary.forEach(book => {
-    let bookCard = document.createElement("div");
-    bookCard.style.marginBottom = "10px";    
-    bookCard.style.border = "1px solid black";
-    bookCard.style.padding = "5px";
-    bookCard.innerHTML = 
-        `Title: ${book.title}<br />
-        Author: ${book.author}<br />
-        Pages: ${book.pages}<br />
-        Read: ${book.read}`
-    bookDisplay.append(bookCard);
-})
+// Display initial books in library
+function populateDisplay() {
+    bookDisplay.innerHTML = "";
+    updateLibraryIndexes();
+    myLibrary.forEach(book => {
+        let bookCard = document.createElement("div");
+        bookCard.style.marginBottom = "10px";    
+        bookCard.style.border = "1px solid black";
+        bookCard.style.padding = "5px";
+        bookCard.innerHTML = 
+            `Title: ${book.title}<br />
+            Author: ${book.author}<br />
+            Pages: ${book.pages}<br />
+            Read: ${book.read}<br />
+            Index: ${book.index}<br />
+            <button class="remove">Remove</button><br />
+            <button class="update">Change Read Status</button>`
+        bookDisplay.append(bookCard);
+    })
+    let removeButton = document.querySelectorAll(".remove");
+    removeButton.forEach(button => {
+        button.addEventListener("click", removeBook, true);
+    })
+}
+
+function removeBook(event){
+    myLibrary.splice(event.target.index - 1, 1);
+    populateDisplay();
+}
+
+function updateLibraryIndexes(){
+    for(let i = 0; i < myLibrary.length; i++){
+        myLibrary[i].index = i + 1;
+    }
+}
 
 let form = document.querySelector(".inputs");
 form.addEventListener("submit", addBookToLibrary, true);
+
+populateDisplay();
